1a12458903bc265dac7d6a2ffa6cf596e9eba87d
Slavisa Avramovic <slavisaavramovic@yahoo.com>
2016-03-08 08:15:49 +0100

    moving the config back to webapp

---
+++ jdsurvey-extcont/src/test/java/com/jd/survey/web/survey/WebApplicationContextTodoControllerTest.java
@@ -0,0 +1,49 @@
+package com.jd.survey.web.survey;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:xml/external/service-context.xml", "classpath:xml/external/webmvc-config.xml"})
+@WebAppConfiguration
+public class WebApplicationContextTodoControllerTest {
+ 
+    private MockMvc mockMvc;
+ 
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+ 
+    @Before
+    public void setUp() {
+        //We have to reset our mock between tests because the mock objects
+        //are managed by the Spring container. If we would not reset them,
+        //stubbing and verified behavior would "leak" from one test to another.
+
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void test() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("redirect:/private"));
+
+    }
+}
